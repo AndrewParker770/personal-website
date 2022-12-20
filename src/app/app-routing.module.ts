7650e89d06c283eca129b3eed25bab93e990f9d9
AndrewParker770 <andrewparker767@yahoo.co.uk>
2022-12-20 18:43:22 +0000

    Create boilerplate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomepageComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'skills', component: SkillsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactComponent } from './contact/contact.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContactComponent,
+    SidenavComponent,
+    ProfileComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
